230a6d1e0395db7cec320ee84465bf95b501cd1f
JANVIER Timothee <Timothee.Janvier@bordeaux-inp.fr>
2021-11-26 17:20:21 +0100

    feat: fetch leclerc website url

--- webscraper/lib/webscraper.dart
+++ webscraper/lib/webscraper.dart
@@ -1,7 +1,7 @@
 import 'package:http/http.dart' as http;
 
 void fetch(){
-  http.get(Uri.parse('https://www.catie.fr'))
+  http.get(Uri.parse('https://www.e.leclerc'))
   .then((response){
     if (response.statusCode == 200) {
       print(response.body);
