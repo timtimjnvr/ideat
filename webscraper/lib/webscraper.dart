36c8b90f879a4d4254a8051f579a6d38e6ea6a95
JANVIER Timothee <Timothee.Janvier@bordeaux-inp.fr>
2021-11-29 14:39:48 +0100

    wip: savings

--- webscraper/lib/webscraper.dart
+++ webscraper/lib/webscraper.dart
@@ -1,10 +1,9 @@
 import 'package:http/http.dart' as http;
 
-void fetch(){
-  http.get(Uri.parse('https://www.e.leclerc'))
-  .then((response){
+void fetch() {
+  http.get(Uri.parse('https://www.e.leclerc')).then((response) {
     if (response.statusCode == 200) {
       print(response.body);
-  }
+    }
   });
 }
