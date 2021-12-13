c379489405ea794658cf14fb28bf3230452bbdb8
JANVIER Timothee <Timothee.Janvier@bordeaux-inp.fr>
2021-12-13 14:49:16 +0100

    feat: product in JSON

--- webscraper/bin/webscraper.dart
+++ webscraper/bin/webscraper.dart
@@ -1,5 +1,5 @@
 import 'package:webscraper/webscraper.dart' as webscraper;
 
 Future<List<webscraper.Product>> main(List<String> arguments) {
-  return webscraper.fetchAmazonProducts("oeuf", "monoprix");
+  return webscraper.fetchAmazonProducts("steak", "monoprix");
 }
--- webscraper/lib/webscraper.dart
+++ webscraper/lib/webscraper.dart
@@ -4,16 +4,18 @@ import 'package:web_scraper/web_scraper.dart';
 import 'dart:convert';
 
 class Product {
-  String? title, price, unityPrice;
-  Product(this.title, this.price, this.unityPrice);
+  String? title, price, unity;
+  Product(this.title, this.price, this.unity);
 
   getProduct() {
-    return {"title": title, "price": price, "unityPrice": unityPrice};
+    return {"title": title, "price": price, "unity": unity};
   }
 }
 
+RegExp pricesRegex = RegExp(r'[0-9]{1,10},[0-9]{1,2}.€\/[^\s,)]{1,10}');
+
 //analyse Product Description to build the product Object
-Product? getProduct(String productDescription) {
+Product getProduct(String productDescription) {
   var tab = productDescription.split("-");
 
   var title = tab[0];
@@ -21,53 +23,52 @@ Product? getProduct(String productDescription) {
 
   //looking for this type of expr : "9,39€ (117,38 €/"
   //the first price is product price and the second unit price (kg/L or unity)
-  RegExp pricesRegex =
-      RegExp(r'[0-9]{1,10},[0-9]{1,2}€.\([0-9]{1,10},[0-9]{1,2}.€');
-
   var stringPriceMatches = pricesRegex.allMatches(restOfProductDescription);
-  var prices = stringPriceMatches.first.group(0);
 
-  if (prices != null) {
-    //separate two prices
-    var tabPrices = prices.split("(");
+  var priceString = stringPriceMatches.first.group(0);
+  var tabPrices = priceString!.split("/");
+  var price = tabPrices[0];
+  var unity = tabPrices[1];
 
-    var price = tabPrices[0];
-    var unitPrice = tabPrices[1];
+  return Product(title, price, unity);
+}
 
-    return Product(title, price, unitPrice);
-  } else {
-    return null;
+Future<List<String>> fetchTitles(
+    String url, String query, String selector) async {
+  List<String> titles = [];
+
+  final webScraper = WebScraper(url);
+
+  if (await webScraper.loadWebPage(query)) {
+    List<Map<String, dynamic>> domElements =
+        webScraper.getElement(selector, ['class']);
+
+    for (var domElement in domElements) {
+      titles.add(domElement['title'] as String);
+    }
   }
+  return titles;
 }
 
 Future<List<Product>> fetchAmazonProducts(
     String productName, String provider) async {
   List<Product> products = [];
 
-  final webScraper = WebScraper('https://www.amazon.fr');
+  List<String> titles = await fetchTitles(
+      'https://www.amazon.fr',
+      '/s?k=' + productName,
+      'div[data-component-type="s-search-result"] > * > span > * > * > *');
 
-  if (await webScraper.loadWebPage('/s?k=' + productName)) {
-    List<Map<String, dynamic>> domElements = webScraper.getElement(
-        'div[data-component-type="s-search-result"] > * > span > * > * > *',
-        ['class']);
-
-    for (var domElement in domElements) {
-      //get only provider's products
-      if ((domElement['title'] as String).toLowerCase().contains(provider)) {
-        var productDescription = domElement['title'] as String;
-
-        Product? product = getProduct(productDescription);
-
-        if (product != null) {
-          products.add(product);
-        }
-      }
+  for (var title in titles) {
+    //get only provider's products
+    if (title.toLowerCase().contains(provider)) {
+      var productDescription = title;
+      try {
+        products.add(getProduct(productDescription));
+      } catch (e) {}
     }
   }
-  //JsonEncoder encoder = new JsonEncoder.withIndent('  ');
-  //String prettyprint = encoder.convert(products);
 
-  products.forEach((product) => print(product.getProduct()));
   return products;
 }
 
@@ -75,8 +76,6 @@ Future<List<Product>> fetchAmazonProducts(
 DIV PRODUCT
 <div data-asin="B07DJB1BW9" data-index="11" data-uuid="b9731ef2-a0d8-4c03-8805-8ca29da2e220" data-component-type="s-search-result" class="sg-col-4-of-12 s-result-item s-asin sg-col-4-of-16 sg-col s-widget-spacing-small sg-col-4-of-20" data-component-id="34" data-cel-widget="search_result_5"><div class="sg-col-inner">
     
-
-
 <span cel_widget_id="MAIN-SEARCH_RESULTS-11" class="celwidget slot=MAIN template=SEARCH_RESULTS widgetId=search-results_5" data-csa-c-id="igl3sh-bqtwe6-tpnezk-eyb6tu" data-cel-widget="MAIN-SEARCH_RESULTS-11">
     <div class="s-expand-height s-include-content-margin s-latency-cf-section s-border-bottom s-border-top"><div class="a-section a-spacing-medium"><div class="a-section a-spacing-none s-grid-status-badge-container s-expand-height s-padding-micro"></div><span data-component-type="s-product-image" class="rush-component"><a class="a-link-normal s-no-outline" href="/LOeuf-nos-Villages-Oeufs-frais/dp/B07DJB1BW9/ref=sr_1_5?__mk_fr_FR=%C3%85M%C3%85%C5%BD%C3%95%C3%91&amp;keywords=oeuf&amp;qid=1638958511&amp;sr=8-5"><div class="a-section aok-relative s-image-square-aspect"><img class="s-image" src="https://m.media-amazon.com/images/I/41t+0lXb-sL._AC_UL320_.jpg" srcset="https://m.media-amazon.com/images/I/41t+0lXb-sL._AC_UL320_.jpg 1x, https://m.media-amazon.com/images/I/41t+0lXb-sL._AC_UL480_QL65_.jpg 1.5x, https://m.media-amazon.com/images/I/41t+0lXb-sL._AC_UL640_QL65_.jpg 2x, https://m.media-amazon.com/images/I/41t+0lXb-sL._AC_UL800_QL65_.jpg 2.5x, https://m.media-amazon.com/images/I/41t+0lXb-sL._AC_UL960_QL65_.jpg 3x" alt="L'Oeuf de nos Villages Oeufs frais plein air - La boîte de 12" data-image-index="5" data-image-load="" data-image-latency="s-product-image" data-image-source-density="1"></div></a></span><div class="a-section a-spacing-none"><div class="a-section a-spacing-none a-spacing-top-small"><h2 class="a-size-mini a-spacing-none a-color-base s-line-clamp-4"><a class="a-link-normal a-text-normal" href="/LOeuf-nos-Villages-Oeufs-frais/dp/B07DJB1BW9/ref=sr_1_5?__mk_fr_FR=%C3%85M%C3%85%C5%BD%C3%95%C3%91&amp;keywords=oeuf&amp;qid=1638958511&amp;sr=8-5"><span class="a-size-base-plus a-color-base a-text-normal">L'Oeuf de nos Villages Oeufs frais plein air - La boîte de 12</span> </a> </h2></div><div class="a-section a-spacing-none a-spacing-top-micro"><div class="a-row a-size-small"><span aria-label="4,6 sur 5&nbsp;étoiles"><span class="a-declarative" data-action="a-popover" data-csa-c-type="widget" data-csa-c-func-deps="aui-da-a-popover" data-a-popover="{&quot;closeButton&quot;:false,&quot;closeButtonLabel&quot;:&quot;&quot;,&quot;position&quot;:&quot;triggerBottom&quot;,&quot;popoverLabel&quot;:&quot;&quot;,&quot;url&quot;:&quot;/review/widgets/average-customer-review/popover/ref=acr_search__popover?ie=UTF8&amp;asin=B07DJB1BW9&amp;ref=acr_search__popover&amp;contextId=search&quot;}" data-csa-c-id="zd4rjp-w6rzly-ml1adf-gje1zd"><a href="javascript:void(0)" role="button" class="a-popover-trigger a-declarative"><i class="a-icon a-icon-star-small a-star-small-4-5 aok-align-bottom"><span class="a-icon-alt">4,6 sur 5&nbsp;étoiles</span></i><i class="a-icon a-icon-popover"></i></a></span> </span><span aria-label="110"><a class="a-link-normal" href="/LOeuf-nos-Villages-Oeufs-frais/dp/B07DJB1BW9/ref=sr_1_5?__mk_fr_FR=%C3%85M%C3%85%C5%BD%C3%95%C3%91&amp;keywords=oeuf&amp;qid=1638958511&amp;sr=8-5#customerReviews"><span class="a-size-base">110</span> </a> </span></div></div><div class="a-section a-spacing-none a-spacing-top-micro"><div class="a-row a-size-base a-color-secondary"><span aria-label="Actuellement indisponible."><span class="a-size-small">Actuellement indisponible.</span></span></div></div></div></div></div>
 </span>
