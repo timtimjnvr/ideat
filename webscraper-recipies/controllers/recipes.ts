c14dc582510757cd260ac5bd635421670513b74c
JANVIER Timothee <Timothee.Janvier@bordeaux-inp.fr>
2022-01-04 12:05:50 +0100

    feat: optional parameters recipe querry

--- webscraper-recipies/controllers/recipes.ts
+++ webscraper-recipies/controllers/recipes.ts
@@ -1,10 +1,12 @@
 import {Request, Response } from "express";
-import getRecipes from "../getRecipes";
+import getRecipes from "../src/getRecipes";
+import recipesConverter from "../src/recipesConverter";
 
 class Recipes {
 
     static async getRecipesHandler(req: Request, res: Response): Promise<void>{
-        const recipes = await getRecipes();
+        const marmitionRecipes = await getRecipes(100);
+        const recipes = await recipesConverter(marmitionRecipes)
         //console.log(recipes);
         res.status(200).send({recipes}).end();
     }
---
+++ webscraper-recipies/src/getRecipes.ts
@@ -0,0 +1,53 @@
+import e = require('express');
+import { searchRecipes, MarmitonQueryBuilder, RECIPE_PRICE, RECIPE_DIFFICULTY, RECIPE_TYPE } from 'marmiton-api';
+
+import {Recipe} from "marmiton-api/dist/types/@types/recipe";
+
+function enumConverter(enumName : any, enumType: String) : any{
+    switch (enumType){
+        case "PRICE":
+            console.log(RECIPE_PRICE);
+            return RECIPE_PRICE[enumName];
+        case "DIFFICULTY":
+            return RECIPE_DIFFICULTY[enumName];
+        case "TYPE":
+            return enumName as RECIPE_TYPE;
+        default:
+            break;
+    }  
+}
+
+class query{
+    qb = new MarmitonQueryBuilder();
+
+    constructor(wordsInRecipe?: string, recipePrice?: string, recipeDifficulty?: string, recipeType?:string, cookingTime?: number){
+        if(wordsInRecipe){
+            this.qb.withTitleContaining(wordsInRecipe)
+        }
+        if(recipePrice){
+            this.qb.withPrice(enumConverter(recipePrice,"PRICE"))
+        }
+        if(cookingTime){
+            this.qb.takingLessThan(cookingTime)
+        }
+        if(recipeDifficulty){
+            this.qb.withDifficulty(enumConverter(recipeDifficulty, "DIFFICULTY"))
+        }
+        if(recipeType){
+            this.qb.withType(enumConverter(recipeType, "TYPE"))
+        }
+    }
+
+    getQuery():string{
+        return this.qb.build();
+    }
+}
+
+const getRecipes = async (numberMax: number) => { 
+    const q = new query('tarte', 'CHEAP',undefined,"dessert",45);
+    
+    return await searchRecipes(q.getQuery(), { limit: numberMax });
+};
+
+export default getRecipes;
+
