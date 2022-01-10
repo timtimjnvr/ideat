3fadc7fbe9dc3385a16d3a63a7c27ffc3a1e98fe
Pierre-Malo <Pierre-Malo.Gaudichau@bordeaux-inp.fr>
2022-01-10 09:34:27 +0100

    feat : new folder system and preferences implementation

--- ideat/lib/main.dart
+++ ideat/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:ideat/home/bottom_nav_bar.dart';
 
 void main() {
   runApp(const MyApp());
@@ -17,65 +18,5 @@ class MyApp extends StatelessWidget {
       ),
       home: const MyHomePage(),
     );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key}) : super(key: key);
-
-  @override
-  State<StatefulWidget> createState() {
-    return MyHomePageState();
-  }
-}
-
-class MyHomePageState extends State<MyHomePage> {
-  int tapCount = 0;
-  int selectedIndex = 0;
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("BottomNavigationBar Example"),
-      ),
-      body: Center(
-          child: Text("Tap Count: " +
-              tapCount.toString() +
-              ", Index: " +
-              selectedIndex.toString())),
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: selectedIndex,
-        selectedIconTheme:
-            const IconThemeData(color: Colors.red, opacity: 1.0, size: 30),
-        backgroundColor: Colors.orange.shade100,
-        unselectedIconTheme:
-            const IconThemeData(color: Colors.cyan, opacity: 1.0, size: 28),
-        items: const [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.add_shopping_cart_rounded),
-            title: Text("Contacts"),
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.search),
-            title: Text("Emails"),
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.person),
-            title: Text("Profile"),
-          )
-        ],
-        onTap: (int index) {
-          onTapHandler(index);
-        },
-      ),
-    );
-  }
-
-  void onTapHandler(int index) {
-    setState(() {
-      tapCount++;
-      selectedIndex = index;
-    });
   }
 }
