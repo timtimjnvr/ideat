d087383970c50466b08641b3e2d71dc964ae282f
JANVIER Timothee <Timothee.Janvier@bordeaux-inp.fr>
2022-01-12 10:33:40 +0100

    feat: fetch recipes architecture

--- orchestrator/bin/orchestrator.dart
+++ orchestrator/bin/orchestrator.dart
@@ -1,8 +1,10 @@
 import 'package:orchestrator/orchestrator.dart' as orchestrator;
 import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 void main(List<String> arguments) async {
   var recipes = await orchestrator.fetchRecipes();
   final respStr = await recipes.stream.bytesToString();
-  print(respStr);
+  final decoded = json.decode(respStr);
+  print(decoded["recipes"]);
 }
--- orchestrator/lib/orchestrator.dart
+++ orchestrator/lib/orchestrator.dart
@@ -1,11 +1,27 @@
 import 'package:http/http.dart' as http;
 
-var client = http.Client();
+class RecipesGetter {
+  final client = http.Client;
+  final numberMax;
 
-Future<http.StreamedResponse> fetchRecipes() {
-  var url = Uri.parse("http://localhost:8888/recipes?numberMax=10");
-  http.Request request = new http.Request("get", url);
-  request.headers.clear();
-  request.headers.addAll({"content-type": "application/json; charset=utf-8"});
-  return request.send();
+  RecipesGetter(this.numberMax);
+
+  Future<http.StreamedResponse> fetchRecipes(String numberMax) {
+    var url = Uri.parse("http://localhost:8888/recipes?numberMax=" + numberMax);
+    http.Request request = new http.Request("get", url);
+    request.headers.clear();
+    request.headers.addAll({"content-type": "application/json; charset=utf-8"});
+    return request.send();
+  }
+}
+
+class RecipesComputer {
+  List<Map<String, String?>> recipesList;
+  int budget;
+
+  RecipesComputer(this.recipesList, this.budget);
+
+  void getPrices() {}
+
+  void recipesMatching(int budget) {}
 }
